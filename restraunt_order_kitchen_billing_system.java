2cf7102ad98ead1813e034f3b787788528fb90db
yasasviramgangavarapu107-oss <yasasviramgangavarapu107@gmail.com>
2026-08-19 19:28:44 +0530

    Add files via upload

---
+++ restraunt_order_kitchen_billing_system.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class restraunt_order_kitchen_billing_system{
+    public static void  main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = "Chicken Biryani";
+        int Chicken_Biryani = 220 ;
+
+        String b = "Chicken Noddels";
+        int Chicken_noddels = 120 ;
+
+
+
+
+    }
+}
